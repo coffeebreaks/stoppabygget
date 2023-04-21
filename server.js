770ddce03ea01ebca1350d25c048f76c395895f3
5t Desktop <stefan@grafikcentralen.se>
2023-04-21 23:53:34 +0200

    asdasd

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World 2!'));
+app.get('/', (req, res) => res.send('Hello World 3!'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
