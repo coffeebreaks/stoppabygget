df7abbe84564321baa6a07f0b52133df408d946c
5t Desktop <stefan@grafikcentralen.se>
2023-04-22 03:35:35 +0200

    asdasd

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const app = express();
 const port = 3000;
 const path = require('path');
 
+const publicPath = './public';
+app.use(express.static(publicPath));
 app.get('/', (req, res) => {
-    const publicPath = path.join(__dirname, 'public');
-    app.use(express.static(publicPath));
 });
 
 app.listen(port, () => console.log(`Example app listening on sport ${port}!`));
